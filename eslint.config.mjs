b2bae6d07b4979ed8d09910177be1c166e49ba8a
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2024-10-10 14:46:15 +0300

    chore: Clean up unnecessary packages
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import globals from 'globals'
-import pluginJs from '@eslint/js'
+import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 export default [
   {languageOptions: {globals: globals.node}},
-  pluginJs.configs.recommended,
+  eslint.configs.recommended,
   ...tseslint.configs.recommended
 ]
