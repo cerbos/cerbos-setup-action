6f4fff14bff356c9ccd5232ed0053f34eef03edb
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2025-09-26 12:43:01 +0300

    enhancement: Use cerbos-actions-common (#219)
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

--- tests/test-utils.test.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2021-2025 Zenauth Ltd.
-// SPDX-License-Identifier: Apache-2.0
-
-import {RunningEnvironment} from '../src/get-running-environment'
-
-export const createRunningEnvironment = (os: string, architecture: string) => {
-  return {architecture, os} as RunningEnvironment
-}
